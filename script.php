129174db125ef7c42499992dca929e0763cb4855
hayder1994ZH <32913953+hayder1994ZH@users.noreply.github.com>
2023-08-22 03:06:21 +0300

    add file db path

--- script.php
+++ script.php
@@ -11,7 +11,7 @@ curl_setopt_array($curl, array(
   CURLOPT_FOLLOWLOCATION => true,
   CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
   CURLOPT_CUSTOMREQUEST => 'POST',
-  CURLOPT_POSTFIELDS => array('chat_id' => $chatId,'document'=> new CURLFILE('test' .'.sql')),
+  CURLOPT_POSTFIELDS => array('chat_id' => $chatId,'document'=> new CURLFILE('/var/www/html/HIS_DB_'. date('Y-m-d') .'_db.sql')),
   // CURLOPT_POSTFIELDS => array('chat_id' => $chatId,'document'=> new CURLFILE(rand() . date('Y-m-d') .'_db.sql')),
 ));
 
