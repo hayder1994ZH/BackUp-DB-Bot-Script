ad884d516e0a979dc8551a611c5312652e4cb84b
hayder1994ZH <32913953+hayder1994ZH@users.noreply.github.com>
2023-08-22 03:02:09 +0300

    --add his config

--- backup_command.php
+++ backup_command.php
@@ -1,3 +1,3 @@
 <?php
-shell_exec('sudo rm -f /var/www/BackUps/NounDb_'. date('Y-m-d',strtotime("-1 days")) .'_db.sql');
-shell_exec('sudo -i -u postgres pg_dump nounTest > /var/www/BackUps/NounDb_'. date('Y-m-d') .'_db.sql');
+shell_exec('sudo rm -rf /var/www/html/HIS_DB_'. date('Y-m-d',strtotime("-1 days")) .'_db.sql');
+shell_exec('mysqldump --defaults-extra-file=/etc/mysql/myPass.cnf -u root his_system > /var/www/html/HIS_DB_'. date('Y-m-d') .'_db.sql');
--- script.php
+++ script.php
@@ -1,7 +1,7 @@
 <?php
 $curl = curl_init();
-$botKey = '';
-$chatId = '';
+$botKey = 'bot6566804080:AAEcBusFjXMioYrT3jclyw7nJ8F0IrXboZI';
+$chatId = '@HisBackUp';
 curl_setopt_array($curl, array(
   CURLOPT_URL => "https://api.telegram.org/$botKey/sendDocument",
   CURLOPT_RETURNTRANSFER => true,
@@ -11,7 +11,8 @@ curl_setopt_array($curl, array(
   CURLOPT_FOLLOWLOCATION => true,
   CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
   CURLOPT_CUSTOMREQUEST => 'POST',
-  CURLOPT_POSTFIELDS => array('chat_id' => $chatId,'document'=> new CURLFILE(rand() . date('Y-m-d') .'_db.sql')),
+  CURLOPT_POSTFIELDS => array('chat_id' => $chatId,'document'=> new CURLFILE('test' .'.sql')),
+  // CURLOPT_POSTFIELDS => array('chat_id' => $chatId,'document'=> new CURLFILE(rand() . date('Y-m-d') .'_db.sql')),
 ));
 
 $response = curl_exec($curl);
